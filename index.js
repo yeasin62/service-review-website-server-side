eead010d12883bca953376d802b2dfa71302d881
yeasin62 <gazi.yeasin@yahoo.com>
2022-11-09 12:53:48 +0600

    integrated mongodb

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json());
 
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://yeasin62:<password>@cluster0.rvsy5g6.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
 app.get('/',(req,res)=> {
     res.send('Server is running');
 })
